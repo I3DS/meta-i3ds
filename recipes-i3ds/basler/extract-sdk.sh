e72ed224cd955c4982898e4616cd1f34a82817d1
Kristoffer Nyborg Gregertsen <kristoffer.gregertsen@sintef.no>
2017-12-14 10:06:32 +0100

    Improves error handling of Pylon SDK extract script

--- recipes-i3ds/basler/extract-sdk.sh
+++ recipes-i3ds/basler/extract-sdk.sh
@@ -1,12 +1,33 @@
 #! /bin/bash
 
+BB="pylon-sdk_5.0.bb"
+
+# The installer must run in the recipe directory
+if [ ! -f "$BB" ]
+then
+    echo "Must run in same directory as $BB"
+    exit 1
+fi
+
+# Check that we get a valid file argument
+if [ -f "$1" ]
+then
+    FILE=$1
+else
+    echo "Usage: ./extract-sdk.sh /path/to/pylon-5.0.*.tar.gz"
+    exit -1
+fi
+
+echo -n "Importing Basler Pylon SDK from $FILE..."
+
 # Create and jump into files directory
 mkdir -p files
-cd files/
 
 # Delete any old files
-rm -f pylon*.tar.gz2
+rm -f files/pylon*.tar.gz2
 
 # Extract Pylon SDK 5.0
-tar -xf $1 --wildcards --no-anchored 'pylonSDK-5.0.*.tar.gz' --strip 1
-mv $(ls pylonSDK-5.0*.tar.gz) pylon-sdk-5.0.tar.gz
+tar -xf $FILE --wildcards --no-anchored 'pylonSDK-5.0.*.tar.gz' --strip 1
+mv $(ls pylonSDK-5.0*.tar.gz) files/pylon-sdk-5.0.tar.gz
+
+echo " [Done]"
